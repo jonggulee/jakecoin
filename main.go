aca1b444b482cf6ed5d6225c37ba123c908ebd11
Jonggu Lee <jg.jake.lee@mz.co.kr>
2023-02-11 12:38:01 +0900

    refactoring get all blockchain blocks

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/jonggu/jakecoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/jonggu/jakecoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PervHash: %s\n\n", block.PrevHash)
+	}
 }
