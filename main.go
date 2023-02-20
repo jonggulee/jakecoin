2e70f3f21da85a5cfa9c17c9a114bf6051558bb9
jonggulee <jg.jake.lee@gmail.com>
2023-02-20 16:40:08 +0900

    All Blocks

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/jonggu/jakecoin/blockchain"
 	"github.com/jonggu/jakecoin/cli"
+	"github.com/jonggu/jakecoin/db"
 )
 
 func main() {
+	defer db.Close()
 	blockchain.Blockchain()
-	// blockchain.Blockchain().AddBlock("First")
-	// blockchain.Blockchain().AddBlock("Second")
-	// blockchain.Blockchain().AddBlock("Third")
 	cli.Start()
 }
--- rest/rest.go
+++ rest/rest.go
@@ -65,14 +65,12 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		return
-		// json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+		json.NewEncoder(rw).Encode((blockchain.Blockchain().Blocks()))
 	case "POST":
-		return
-		// var addBlockBody addBlockBody
-		// utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		// blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
-		// rw.WriteHeader(http.StatusCreated)
+		var addBlockBody addBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.Blockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
 	}
 }
 
