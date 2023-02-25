5537781e0b46856d9b404c86ab7d819ef49db7c2
jonggulee <jg.jake.lee@gmail.com>
2023-02-25 14:43:27 +0900

    Adding Mine Block

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/jonggu/jakecoin/cli"
+	"github.com/jonggu/jakecoin/db"
 )
 
 func main() {
-	// defer db.Close()
-	// blockchain.Blockchain()
-	// cli.Start()
-
-	difficutly := 5
-	target := strings.Repeat("0", difficutly)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("Hello"+fmt.Sprint(nonce))))
-		fmt.Printf("Hash: %s\nTarget: %s\nNonce: %d\n\n", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			fmt.Println(nonce)
-			return
-		} else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
