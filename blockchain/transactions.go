6b47df22a005a3decd8acee6021683e9e58e5ae3
jonggulee <jg.jake.lee@gmail.com>
2023-03-01 16:15:24 +0900

    Added AddTx

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"errors"
 	"time"
 
 	"github.com/jonggu/jakecoin/utils"
@@ -9,6 +10,12 @@ import (
 const (
 	minerReward int = 50
 )
+
+type mempool struct {
+	Txs []*Tx
+}
+
+var Mempool *mempool = &mempool{}
 
 type Tx struct {
 	Id        string   `json:"id"`
@@ -46,4 +53,20 @@ func makeCoinbaseTx(address string) *Tx {
 	}
 	tx.getID()
 	return &tx
+}
+
+func makeTx(from, to string, amout int) (*Tx, error) {
+	if Blockchain().BalanceByAddress(from) < amout {
+		return nil, errors.New("not enough money")
+	}
+}
+
+func (m *mempool) AddTx(to string, amount int) error {
+	tx, err := makeTx("jake", to, amount)
+	// utils.HandleErr(err)
+	if err != nil {
+		return err
+	}
+	m.Txs = append(m.Txs, tx)
+	return nil
 }
