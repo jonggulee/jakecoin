a4232f96604dbd7418003623c29631e7ecefae6b
jonggulee <jg.jake.lee@gmail.com>
2023-03-09 10:49:08 +0900

    Test signing messages

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"github.com/jonggu/jakecoin/cli"
-	"github.com/jonggu/jakecoin/db"
-)
+import "github.com/jonggu/jakecoin/wallet"
 
 func main() {
-	defer db.Close()
-	cli.Start()
+	// defer db.Close()
+	// cli.Start()
+	wallet.Start()
 }
